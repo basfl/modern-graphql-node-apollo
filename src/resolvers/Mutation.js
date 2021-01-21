bf9724a00a6613b470a12b2add7bddec4ab094b6
unknown <jupiter85838@yahoo.com>
2021-01-21 18:33:37 -0500

    add login mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -26,6 +26,32 @@ const Mutation = {
         })
 
     },
+    async login(parrent, args, {
+        prisma
+    }, info) {
+
+        const user = await prisma.query.user({
+            where: {
+                email: args.data.email
+            }
+        });
+        if (!user) {
+            throw new("unable to login !");
+        }
+        const isMatch = await bcrypt.compare(args.data.password, user.password);
+        if (!isMatch) {
+            throw new("unable to login  !");
+        }
+
+        return ({
+            user,
+            token: jwt.sign({
+                id: user.id
+            }, "thisisasecret")
+        });
+
+
+    },
     async updateUser(parent, args, {
         prisma
     }, info) {
