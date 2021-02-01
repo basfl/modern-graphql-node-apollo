02e8fb0171d46ef6e777200895d40728b440882d
unknown <jupiter85838@yahoo.com>
2021-02-01 12:17:45 -0500

    add sone jest test cases

---
+++ src/utils/user.js
@@ -0,0 +1,13 @@
+const getFirstName = (name) => {
+    return name.split(" ")[0];
+}
+
+const isValidPassword = (password) => {
+    return password.length > 8 && !password.toLowerCase().includes("password")
+}
+
+
+export {
+    getFirstName,
+    isValidPassword
+}
