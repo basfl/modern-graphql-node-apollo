92ab0d407f2ab7d07feaf3a80b53bd9e57da2786
unknown <jupiter85838@yahoo.com>
2021-02-05 09:32:18 -0500

    refactoring tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,60 +1,23 @@
-import 'cross-fetch/polyfill';
-import ApolloBoost, {
-    gql
-} from 'apollo-boost';
-import prisma from '../src/prisma';
-import bcrypt from 'bcryptjs'
+import 'cross-fetch/polyfill'
+import { gql } from 'apollo-boost'
+import prisma from '../src/prisma'
+import seedDatabase from './utils/seedDatabase'
+import getClient from './utils/getClient'
 
-const client = new ApolloBoost({
-    uri: 'http://localhost:4000'
-})
+const client = getClient()
 
-beforeEach(async () => {
-    await prisma.mutation.deleteManyPosts();
-    await prisma.mutation.deleteManyUsers();
-    const user = await prisma.mutation.createUser({
-        data: {
-            name: 'ben',
-            email: 'ben@example.com',
-            password: bcrypt.hashSync('Red098!@#$')
-        }
-    });
-    await prisma.mutation.createPost({
-        data: {
-            title: 'My published post',
-            body: '',
-            published: true,
-            author: {
-                connect: {
-                    id: user.id
-                }
-            }
-        }
-    });
-    await prisma.mutation.createPost({
-        data: {
-            title: 'My draft post',
-            body: '',
-            published: false,
-            author: {
-                connect: {
-                    id: user.id
-                }
-            }
-        }
-    });
-})
+beforeEach(seedDatabase)
 
 test('Should create a new user', async () => {
-    const createUser = gql `
+    const createUser = gql`
         mutation {
             createUser(
                 data: {
-                    name: "testUser1",
-                    email: "testUser1@example.com",
+                    name: "parisa",
+                    email: "parisa@example.com",
                     password: "MyPass123"
                 }
-            ){ 
+            ){
                 token,
                 user {
                     id
@@ -66,15 +29,13 @@ test('Should create a new user', async () => {
     const response = await client.mutate({
         mutation: createUser
     })
-    console.log("test response", response);
-    const exists = await prisma.exists.User({
-        id: response.data.createUser.user.id
-    })
+
+    const exists = await prisma.exists.User({ id: response.data.createUser.user.id })
     expect(exists).toBe(true)
-}, 30000);
+})
 
 test('Should expose public author profiles', async () => {
-    const getUsers = gql `
+    const getUsers = gql`
         query {
             users {
                 id
@@ -83,30 +44,48 @@ test('Should expose public author profiles', async () => {
             }
         }
     `
-    const response = await client.query({
-        query: getUsers
-    })
+    const response = await client.query({ query: getUsers })
 
     expect(response.data.users.length).toBe(1)
     expect(response.data.users[0].email).toBe(null)
     expect(response.data.users[0].name).toBe('ben')
-});
+})
 
-test('Should expose published posts', async () => {
-    const getPosts = gql `
-        query {
-            posts {
-                id
-                title
-                body
-                published
+test('Should not login with bad credentials', async () => {
+    const login = gql`
+        mutation {
+            login(
+                data: {
+                    email: "ben@example.com",
+                    password: "red098!@#$"
+                }
+            ){
+                token
             }
         }
     `
-    const response = await client.query({
-        query: getPosts
-    })
 
-    expect(response.data.posts.length).toBe(1)
-    expect(response.data.posts[0].published).toBe(true)
-});
+    await expect(
+        client.mutate({ mutation: login })
+    ).rejects.toThrow()
+})
+
+test('Should not signup user with invalid password', async () => {
+    const createUser = gql`
+        mutation {
+            createUser(
+                data: {
+                    name: "parisa",
+                    email: "parisa@example.com",
+                    password: "pass"
+                }
+            ){
+                token
+            }
+        }
+    `
+
+    await expect(
+        client.mutate({ mutation: createUser })
+    ).rejects.toThrow()
+})
